34ad4571858535d4ed760f08f95305a50a3cfcff
test <test>
2023-12-20 01:45:12 +0700

    test

---
+++ bank.py
@@ -0,0 +1,9 @@
+answer = (input("Приветствие: "))
+
+if answer:
+    if answer.startswith("здравствуйте"):
+        print ("$0")
+    elif  answer.startswith("з"):
+        print("$20")
+    elif _:
+        print ("$100")
